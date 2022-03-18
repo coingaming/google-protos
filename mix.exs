392b283f1475417f68f2f357818b9ae68569ac2e
Reio Piller <reio@heathmont.net>
2022-03-19 00:50:29 +0200

    fix: semantic-release

--- mix.exs
+++ mix.exs
@@ -1,16 +1,28 @@
 defmodule GoogleProtos.MixProject do
   use Mix.Project
 
+  @version (case File.read("VERSION") do
+    {:ok, version} -> String.trim(version)
+    {:error, _} -> "0.0.0-development"
+  end)
+
   def project do
     [
       app: :google_protos,
       name: "Google Protos",
-      version: "0.2.0",
+      version: @version,
       elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: "Protos by Google",
-      package: package()
+      package: package(),
+      # docs
+      name: "Exlnd",
+      source_url: "https://github.com/coingaming/google-protos",
+      homepage_url: "https://github.com/coingaming/google-protos/tree/v#{@version}",
+      docs: [
+        source_ref: "v#{@version}"
+      ]
     ]
   end
 
@@ -20,16 +32,17 @@ defmodule GoogleProtos.MixProject do
 
   defp deps do
     [
-      {:protobuf, "~> 0.8"},
-      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:protobuf, "~> 0.9", organization: "coingaming"},
+      {:ex_doc, ">= 0.0.0", only: [:dev, :test], runtime: false},
     ]
   end
 
   defp package do
     [
+      organization: "coingaming",
       maintainers: ["Tony Han"],
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/tony612/google-protos"},
+      links: %{"GitHub" => "https://github.com/coingaming/google-protos/tree/v#{@version}"},
       files: ~w(mix.exs README.md lib config LICENSE)
     ]
   end
